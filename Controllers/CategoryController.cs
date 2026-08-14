97d965455e8ccc639d265dcee9b48f54318af915
Administrator <Administrator@SRKI-151>
2026-08-14 10:37:22 +0530

    view

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movie_Management_System.Models;
+
+namespace Movie_Management_System.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        public ActionResult Create(MovieCategory cat)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // TODO: Add insert logic here
+                    string ConnectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ToString();
+                    SqlConnection connection = new SqlConnection(ConnectionString);
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("Insert_Cat", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Cat_type", cat.Cat_type);
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                    ModelState.Clear();
+                    ViewBag.Message = "Category inserted successfully!";
+                    return View(new MovieCategory());
+                }
+                return View(cat);
+            }
+            catch
+            {
+                ViewBag.Message = "Error while inserting category.";
+                return View();
+            }
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,123 @@
+﻿using Movie_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie_Management_System.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ToString();
+            SqlConnection connection = new SqlConnection(connectionString);
+            List<SelectListItem> list = new List<SelectListItem>();
+            SqlCommand cmd = new SqlCommand("Bind_Category", connection);
+            cmd.CommandType = CommandType.Text;
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                list.Add(new SelectListItem { Text = reader["Cat_type"].ToString(), Value = reader["Cat_type"].ToString() });
+            }
+            ViewBag.CategoryList = list;
+            return View(new movie());
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        public ActionResult Create(movie mov)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ToString();
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    SqlCommand cmd = new SqlCommand("Insert_Movie", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.Parameters.AddWithValue("@Movie_name", mov.Movie_name);
+                    cmd.Parameters.AddWithValue("@Release_date", mov.Release_date);
+                    cmd.Parameters.AddWithValue("@Cat_id", mov.Cat_id);
+                    cmd.Parameters.AddWithValue("@Rate", mov.Rate);
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                    ModelState.Clear();
+                    ViewBag.Message = "Movie Insert Successflly!";
+                    return View(new movie());
+                }
+                // TODO: Add insert logic here
+
+                return View(new movie());
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex + " :Error while inserting category.";
+                return View(mov);
+            }
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
