fd23f6e06a49a98ac9efe0d0875dba7c462000a8
Administrator <Administrator@SRKI-151>
2026-08-14 11:04:51 +0530

    view

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,112 @@
+﻿using Movie_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie_Management_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Login/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Login/Create
+        [HttpPost]
+        public ActionResult Create(user user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return View(user);
+                }
+                string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ToString();
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                SqlCommand cmd = new SqlCommand(connectionString, connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Email_id", user.Email_id);
+                cmd.Parameters.AddWithValue("@User_password", user.User_password);
+                connection.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.Read())
+                {
+                    ViewBag.Email_id = sdr["Email_id"].ToString();
+                    ViewBag.User_Passowrd = sdr["user_password"].ToString();
+                    return View("index");
+                }
+                ViewBag.Message = "Invaid";
+                return View(user);
+                // TODO: Add insert logic here
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Login/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Login/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Login/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Login/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
